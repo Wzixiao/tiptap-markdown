a89da0a739adbdffdda83f53f97de4e7bf3a143e
Ran <redamancy0326@foxmail.com>
2025-05-22 19:51:53 +0800

    fix: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -14,6 +14,7 @@ export interface MarkdownOptions  {
     breaks?: Boolean,
     transformPastedText?: Boolean,
     transformCopiedText?: Boolean,
+    ignoreRegex: String[]
 }
 
 export interface MarkdownStorage {
